539de3250b2c640b948218649590160c0e490640
Manohar Kanapaka <manohar.kanapaka@gmail.com>
2024-09-11 23:44:57 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app with shadcn UI</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
